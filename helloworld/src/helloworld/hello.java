192b6e977046c45804d6b4472dab80e84b6742e5
Duque95 <alberto.duque31@gmail.com>
2022-09-26 13:28:04 -0700

    Init commit

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" My name is Alberto");
+		System.out.println(" i am 27 years old");
+		System.out.println(" My home is in Gerigia");
+		System.out.println();
+			// hello
+		// update
+	}
+
+}
---
+++ helloworld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Alberto Duque
+ *
+ */
+module helloworld {
+}
